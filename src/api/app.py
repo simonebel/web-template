0a632921ca713b78e3f2570d78ea07520a838239
simon <ebel.simon@hotmail.com>
2023-11-21 23:30:38 +0100

    refactor: clean api main

--- src/api/app.py
+++ src/api/app.py
@@ -1,26 +1,11 @@
-import multiprocessing
-import os
-from argparse import ArgumentParser
-from pathlib import Path
-from typing import Any, Callable, Dict
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 from api.test import router as test_router
 
 app = FastAPI()
 
 origins = [
-    "https://cedar-rf.saclay.inria.fr",
-    "http://cedar-rf.saclay.inria.fr",
-    "https://cedar-rf.saclay.inria.fr:8081",
-    "http://cedar-rf.saclay.inria.fr:8081",
-    "https://cedar-rf.saclay.inria.fr:8002",
-    "http://cedar-rf.saclay.inria.fr:8002",
-    "http://cedar-rf.saclay.inria.fr:8001",
-    "https://cedar-rf.saclay.inria.fr:8001",
     "http://localhost:3000",
     "localhost:3000",
 ]
